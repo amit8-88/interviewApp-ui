cd413dcd3d71c162febfb7cb9767d95440176139
Amit Kumar <amit8.88@gmail.com>
2018-06-13 13:02:37 +0530

    Intial Commit

---
+++ src/main/java/com/softvision/inverviewAppui/InverviewAppUiApplication.java
@@ -0,0 +1,12 @@
+package com.softvision.inverviewAppui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InverviewAppUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InverviewAppUiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/softvision/inverviewAppui/InverviewAppUiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.softvision.inverviewAppui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InverviewAppUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
